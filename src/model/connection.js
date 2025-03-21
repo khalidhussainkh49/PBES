7088786de5677bc090e7fe5fca38fa1cc080ee3d
alfarukky <alfarukky@gmail.com>
2025-03-21 22:39:44 +0400

    new folder created and all files and folders moved into it

--- src/model/connection.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-export const mongoConnect = async (MONGO_URI) => {
-  if (!MONGO_URI) {
-    throw new Error('MongoDB URI is not provided');
-  }
-  return mongoose.connect(MONGO_URI);
-};
