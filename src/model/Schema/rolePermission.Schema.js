49e38682371fa7e333f129ee970254e5122b340b
alfarukky <alfarukky@gmail.com>
2025-03-21 13:45:24 +0400

    Schemas for user and roles created

---
+++ src/model/Schema/rolePermission.Schema.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const rolePermissionSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    enum: ['superadmin', 'admin', 'officer'], // Roles are defined here
+    required: true,
+    unique: true,
+  },
+  permissions: {
+    create: { type: Boolean, default: false },
+    read: { type: Boolean, default: false },
+    update: { type: Boolean, default: false },
+    delete: { type: Boolean, default: false },
+    manageUsers: { type: Boolean, default: false },
+  },
+});
+
+export default mongoose.model('RolePermission', rolePermissionSchema);
---
+++ src/model/Schema/user.Schema.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  serviceNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['superadmin', 'admin', 'officer'],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Middleware to update the 'updatedAt' field before saving
+userSchema.pre('save', function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+export default mongoose.model('User', userSchema);
